d03c9b173b943128d9eef3e87c3e0da0fe924d6b
Melissa Vaiden <melissavaiden@gmail.com>
2022-12-20 17:42:03 -0500

    commit tree c545a0747fcd801a768e15f54261e9ab11d24504

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ['Fiction', 'Non-Fiction']
+    validate :clickbate_titles
+
+    
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+      ]
+
+
+    def clickbate_titles
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title } 
+            errors.add(:title, "Title not allowed")
+        end
+    end
 end
